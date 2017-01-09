e25ea97a943b9d69d3287b7497c80acb8726dd03
luanknooc <luanknooc@gmail.com>
2017-01-09 15:46:14 +0700

    update 9/1

--- js/main.js
+++ js/main.js
@@ -1,56 +1,19 @@
 $(document).ready(function(){
 	
-	//To Top
-	if($('#to-top').length){
-		var toTop = function(){
-			if($(window).scrollTop() > 1){
-				$('#to-top').css({'bottom':'-20px'});
-			}else{
-				$('#to-top').css({'bottom':'-110px'});
-			}
-		};
-		toTop();
-		$(window).on('scroll', function(){
-			toTop();
-		});
-		
-		$('#to-top').on('click', function(e){
-			event.preventDefault();
-			$('html,body').animate({scrollTop : 0}, 600);
-		});
-	}
-	
-	//Animate anchor link
-	$('.navbar-nav a').on('click', function(e){
-		event.preventDefault();
-		$('html,body').animate({
-			scrollTop: $($(this).attr('href')).offset().top()
-		}, 800);
-	});
+	$(document).ready( function() {
+	    /* Check width on page load*/
+	    if ( $(window).width() < 540) {
+	     $('.two').removeClass('wow');
+	    }
+	    else {}
+	 });
 
-	
-	//Popup
-	$('#priceList img').on('click', function(e){
-		event.preventDefault();
-		$('.popup').addClass('visible');
-	});
-	
-	$('.popup').on('click', function(e){
-		event.preventDefault();
-		if($(event.target).is('.popup') || $(event.target).is('.close')) {
-			$('.popup').removeClass('visible');
-		}
-	});
-	
-	$(document).keyup(function(){
-		if(event.which=='27'){
-			$('.popup').removeClass('visible');
-		}
-	});
-	
-	//Margin Popup
-	var marTop = ($('.popup-container').height() + 24) / 2; 
-	var marLeft = ($('.popup-container').width() + 24) / 2;
-	$('.popup-container').css({'margin-top':-marTop, 'margin-left':-marLeft});
+	 $(window).resize(function() {
+	    /*If browser resized, check width again */
+	    if ($(window).width() < 550) {
+	     $('.two').removeClass('wow');
+	    }
+	    else {$('.two').addClass('mobile');}
+	 });
 	
 });
